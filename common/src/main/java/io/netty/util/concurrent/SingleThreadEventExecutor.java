0540a9f3110f5dde4b288a3ccdbc625d8a14ab3e
亚克(蒋飞) <yake.jiangf@goparcel.cn>
2020-05-26 23:41:05 +0800

    SingleThread Executor

--- common/src/main/java/io/netty/util/concurrent/SingleThreadEventExecutor.java
+++ common/src/main/java/io/netty/util/concurrent/SingleThreadEventExecutor.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.LinkedHashSet;
 import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
@@ -109,6 +106,128 @@ public abstract class SingleThreadEventExecutor extends AbstractScheduledEventEx
         return new LinkedBlockingQueue<Runnable>(maxPendingTasks);
     }
 
+    protected void addTask(Runnable task) {
+        ObjectUtil.checkNotNull(task, "task");
+        if (!offerTask(task)) {
+            reject(task);
+        }
+    }
+
+    final boolean offerTask(Runnable task) {
+        if (isShutdown()) {
+            reject();
+        }
+        return taskQueue.offer(task);
+    }
+
+    protected boolean removeTask(Runnable task) {
+        return taskQueue.remove(ObjectUtil.checkNotNull(task, "task"));
+    }
+
+    protected void updateLastExecutionTime() {
+        //最新执行时间
+        lastExecutionTime = ScheduledFutureTask.nanoTime();
+    }
+
+    /**
+     * Run the tasks in the {@link #taskQueue}
+     */
+    protected abstract void run();
+
+    /**
+     * Do nothing, sub-classes may override
+     */
+    protected void cleanup() {
+        // NOOP
+    }
+
+    protected void wakeup(boolean inEventLoop) {
+        if (!inEventLoop) {
+            // Use offer as we actually only need this to unblock the thread and if offer fails we do not care as there
+            // is already something in the queue.
+            taskQueue.offer(WAKEUP_TASK);
+        }
+    }
+
+    @Override
+    public boolean inEventLoop(Thread thread) {
+        return thread == this.thread;
+    }
+
+    @Override
+    public boolean isShuttingDown() {
+        return state >= ST_SHUTTING_DOWN;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return state >= ST_SHUTDOWN;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return state == ST_TERMINATED;
+    }
+
+    /**
+     * 确认是否关闭
+     *
+     * @return
+     */
+    protected boolean confirmShutdown() {
+        if (!isShuttingDown()) {
+            return false;
+        }
+
+        if (!inEventLoop()) {
+            throw new IllegalStateException("must be invoked from an event loop");
+        }
+
+        cancelScheduledTasks();
+
+        if (gracefulShutdownStartTime == 0) {
+            gracefulShutdownStartTime = ScheduledFutureTask.nanoTime();
+        }
+
+        if (runAllTasks() || runShutdownHooks()) {
+            if (isShutdown()) {
+                // Executor shut down - no new tasks anymore.
+                return true;
+            }
+
+            // There were tasks in the queue. Wait a little bit more until no tasks are queued for the quiet period or
+            // terminate if the quiet period is 0.
+            // See https://github.com/netty/netty/issues/4241
+            if (gracefulShutdownQuietPeriod == 0) {
+                return true;
+            }
+            taskQueue.offer(WAKEUP_TASK);
+            return false;
+        }
+
+        final long nanoTime = ScheduledFutureTask.nanoTime();
+
+        if (isShutdown() || nanoTime - gracefulShutdownStartTime > gracefulShutdownTimeout) {
+            return true;
+        }
+
+        if (nanoTime - lastExecutionTime <= gracefulShutdownQuietPeriod) {
+            // Check if any tasks were added to the queue every 100ms.
+            // TODO: Change the behavior of takeTask() so that it returns on timeout.
+            taskQueue.offer(WAKEUP_TASK);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // Ignore
+            }
+
+            return false;
+        }
+
+        // No tasks were added for last quiet period - hopefully safe to shut down.
+        // (Hopefully because we really cannot make a guarantee that there will be no execute() calls by a user.)
+        return true;
+    }
 
     @Override
     public void execute(Runnable task) {
@@ -116,9 +235,151 @@ public abstract class SingleThreadEventExecutor extends AbstractScheduledEventEx
         execute(task, !(task instanceof LazyRunnable) && wakesUpForTask(task));
     }
 
+    @Override
+    public void lazyExecute(Runnable task) {
+        execute(ObjectUtil.checkNotNull(task, "task"), false);
+    }
+
+    private void execute(Runnable task, boolean immediate) {
+        boolean inEventLoop = inEventLoop();
+        addTask(task);
+        if (!inEventLoop) {
+            // 不在启动线程
+            startThread();
+            if (isShutdown()) {
+                // 是否关闭
+                boolean reject = false;
+                try {
+                    if (removeTask(task)) {
+                        reject = true;
+                    }
+                } catch (UnsupportedOperationException e) {
+                }
+                if (reject) {
+                    reject();
+                }
+            }
+        }
+
+        if (!addTaskWakesUp && immediate) {
+            wakeup(inEventLoop);
+        }
+    }
+
 
     protected boolean wakesUpForTask(Runnable task) {
         return true;
+    }
+
+    /**
+     * 拒绝
+     */
+    protected static void reject() {
+        throw new RejectedExecutionException("event executor terminated");
+    }
+
+    protected final void reject(Runnable task) {
+        rejectedExecutionHandler.rejected(task, this);
+    }
+
+    private static final long SCHEDULE_PURGE_INTERVAL = TimeUnit.SECONDS.toNanos(1);
+
+    //启动
+    private void startThread() {
+        if (state == ST_NOT_STARTED) {
+            if (STATE_UPDATER.compareAndSet(this, ST_NOT_STARTED, ST_STARTED)) {
+                boolean success = false;
+                try {
+                    doStartThread();
+                    success = true;
+                } finally {
+                    if (!success) {
+                        STATE_UPDATER.compareAndSet(this, ST_STARTED, ST_NOT_STARTED);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void doStartThread() {
+        assert thread == null;
+        //提交一个线程启动
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                //启动线程
+                thread = Thread.currentThread();
+                if (interrupted) {
+                    thread.interrupt();
+                }
+
+                boolean success = false;
+                updateLastExecutionTime();
+                try {
+                    //执行run 方法
+                    SingleThreadEventExecutor.this.run();
+                    success = true;
+                } catch (Throwable t) {
+                    log.warn("Unexpected exception from an event executor: ", t);
+                } finally {
+                    //执行结束一般关闭 executor
+                    for (; ; ) {
+                        int oldState = state;
+                        if (oldState >= ST_SHUTTING_DOWN || STATE_UPDATER.compareAndSet(
+                                SingleThreadEventExecutor.this, oldState, ST_SHUTTING_DOWN)) {
+                            break;
+                        }
+                    }
+
+                    try {
+                        for (; ; ) {
+                            if (confirmShutdown()) {
+                                break;
+                            }
+                        }
+
+                        for (; ; ) {
+                            int oldState = state;
+                            if (oldState >= ST_SHUTDOWN || STATE_UPDATER.compareAndSet(
+                                    SingleThreadEventExecutor.this, oldState, ST_SHUTDOWN)) {
+                                break;
+                            }
+                        }
+
+                        confirmShutdown();
+                    } finally {
+                        try {
+                            cleanup();
+                        } finally {
+                            FastThreadLocal.removeAll();
+
+                            STATE_UPDATER.set(SingleThreadEventExecutor.this, ST_TERMINATED);
+                            threadLock.countDown();
+                            int numUserTasks = drainTasks();
+                            terminationFuture.setSuccess(null);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+
+    final int drainTasks() {
+        int numTasks = 0;
+        for (; ; ) {
+            Runnable runnable = taskQueue.poll();
+            if (runnable == null) {
+                break;
+            }
+            // WAKEUP_TASK should be just discarded as these are added internally.
+            // The important bit is that we not have any user tasks left.
+            if (WAKEUP_TASK != runnable) {
+                numTasks++;
+            }
+        }
+        return numTasks;
     }
 
     private static final class DefaultThreadProperties implements ThreadProperties {
